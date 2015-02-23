f1c240b6caa9f4451c601ebeb8b0de185a79db32
ms2666 <ms2666@cornell.edu>
2015-02-23 13:33:46 -0500

    edited the program from boris

--- MATLAB/netCDF testing/fromBoris.m
+++ MATLAB/netCDF testing/fromBoris.m
@@ -2,7 +2,7 @@ function fromBoris(filename)
     close all;
 
     if nargin<1
-        filename='uv20150101rt.nc';
+        filename='uv20150102rt.nc';
     end
     
     flowData=readFile(filename);
@@ -14,7 +14,6 @@ function fromBoris(filename)
     flowData.lon=[flowData.lon(eastBndInd:lonBndInd)-360; flowData.lon(1:eastBndInd-1)];
     
     [x,y]=meshgrid(flowData.lon,fliplr(flowData.lat));
-
     
     u=[flowData.u(eastBndInd:lonBndInd,:,1,1); flowData.u(1:eastBndInd-1,:,1,1)];
     v=[flowData.v(eastBndInd:lonBndInd,:,1,1); flowData.v(1:eastBndInd-1,:,1,1)];
@@ -51,11 +50,11 @@ function flowData=readFile(filename)
     flowData=struct;
     
     flowData.time=ncread(filename,'time');
-    flowData.depth=ncread(filename,'depth');
+    flowData.depth=ncread(filename,'zlev');
     flowData.lat=ncread(filename,'lat');
     flowData.lon=ncread(filename,'lon');
     flowData.u=ncread(filename,'u');
     flowData.v=ncread(filename,'v');
-    flowData.u_anom=ncread(filename,'u_anom');
-    flowData.v_anom=ncread(filename,'v_anom');
+%     flowData.u_anom=ncread(filename,'u_anom');
+%     flowData.v_anom=ncread(filename,'v_anom');
 end
