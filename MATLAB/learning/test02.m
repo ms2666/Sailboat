b26ddb301db7bdae597a2aec5df82bb066037adb
ms2666 <ms2666@cornell.edu>
2015-02-23 19:45:45 -0500

    small changes to test02

--- MATLAB/learning/test02.m
+++ MATLAB/learning/test02.m
@@ -2,22 +2,12 @@
 step_size = 10;
 bounds = round([-83.2,13.3,2.4,64.7]);
 
-Xs = bounds(1):step_size:bounds(3);
-Ys = bounds(2):step_size:bounds(4);
-clear step_size; clear bounds;
-[~, xLen] = size(Xs);
-[~, yLen] = size(Ys);
-
-% generate coordinates based on bounding box
-[p,q] = meshgrid(Xs, Ys);
-coordinates = [p(:) q(:)];
-% clear p; clear q;
+[coordinates, n] = genCoord(bounds, step_size);
 
 scatter(coordinates(:,1), coordinates(:,2))
 axis square
 
 % create set of nodes
-n = xLen * yLen;
 nodeList = (1:n)';
 nodeMatrix = [nodeList coordinates];
 adjMat = adj_matrix(nodeMatrix);
