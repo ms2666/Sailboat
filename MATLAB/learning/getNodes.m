350e9c8eaed6d5ae47ddf9bae5b28cc9e34f156d
ms2666 <ms2666@cornell.edu>
2015-02-24 15:58:29 -0500

    direct travel
    now you can go from a city to city without bothering about which nodes
    to choose

---
+++ MATLAB/learning/getNodes.m
@@ -0,0 +1,26 @@
+function [finalS, finalD] = getNodes(trueSource, trueDestination, nodeMatrix)
+% estimates node locations
+
+possibleXs = nodeMatrix(:, 2);
+possibleYs = nodeMatrix(:, 3);
+
+Xs = [trueSource(2) trueDestination(2)];
+Ys = [trueSource(1) trueDestination(1)];
+
+xInd = knnsearch(possibleXs, Xs');
+yInd = knnsearch(possibleYs, Ys');
+
+Xs = nodeMatrix(xInd, 2);
+Ys = nodeMatrix(yInd, 3);
+
+for i = 1:size(nodeMatrix, 1)
+    if (nodeMatrix(i, 2) == Xs(2)) && (nodeMatrix(i, 3) == Ys(2))
+        finalD = i;
+    end
+    
+    if (nodeMatrix(i, 2) == Xs(1)) && (nodeMatrix(i, 3) == Ys(1))
+        finalS = i;
+    end
+    
+    
+end
--- MATLAB/learning/graphPath.m
+++ MATLAB/learning/graphPath.m
@@ -1,4 +1,4 @@
-function [] = graphPath(nodeMatrix, source, destination)
+function [path] = graphPath(nodeMatrix, source, destination)
 % calculate and graph the shortest path
 
 sparseAdjMat = sparse(adj_matrix(nodeMatrix));
@@ -13,14 +13,20 @@ end
 % calculate shortest path
 [~, path, ~] = graphshortestpath(sparseAdjMat, source, destination);
 
-path
-
 % plot shortest path
 for i = 1:(size(path, 2)-1)
     X = [nodeMatrix(path(i), 2) nodeMatrix(path(i+1), 2)];
     Y = [nodeMatrix(path(i), 3) nodeMatrix(path(i+1), 3)];
     plot(X,Y, 'r-', 'LineWidth', 4);
 end
-axis equal
+
+% draw map
+coast=load('coast');
+plot(coast.long, coast.lat)
+ylim([10 70])
+xlim([-100 10])
+
+
+% axis equal
 
 end
