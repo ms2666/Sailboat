580a78e5dcad7c70e35beeaf7d9bbc0831f5d363
ms2666 <ms2666@cornell.edu>
2015-02-27 17:40:27 -0500

    changed weighing function

--- MATLAB/learning/calculateWeight.m
+++ MATLAB/learning/calculateWeight.m
@@ -4,5 +4,5 @@ xDiff = nodeMatrix(r, 2) - nodeMatrix(c, 2);
 yDiff = nodeMatrix(r, 3) - nodeMatrix(c, 3);
 weight = sqrt(yDiff^2 + xDiff^2);
 
-weight = 100000*rand*weight;
+% weight = 100000*rand*weight;
 end
