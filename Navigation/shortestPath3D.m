1332e1f01214604d8af89580ac14506af4f0aeed
ms2666 <ms2666@cornell.edu>
2015-03-17 18:55:22 -0400

    goes from Israel to Japan

--- Navigation/shortestPath3D.m
+++ Navigation/shortestPath3D.m
@@ -9,18 +9,20 @@ MexicoCity = [19.4, -99.1];
 Houston = [31.0, -100];
 TelAviv = [32.1, 34.8];
 Barcelona = [41.2, 2.2];
+Osaka = [34.6, 135.5];
 
 % bounding box for the atlantic ocean
 NorthAtlantic = [-83.2,22.0,0.3,58.8];
 Mediterranean = [-11.4,28.1,45.8,49.9];
 Custom = [-100,10,0.3,58.8];
 Custom2 = [-104.1,12.2,45.6,55.2];
+Isreal2Japan = [-28.3,-39.9,162.0,47.8];
 
 %% Inputs to program
 % resolution is the number of degrees between each node
-resolution = 1;
+resolution = 2;
 % bounding box for nodes. smaller = faster
-bounds = Custom2;
+bounds = Isreal2Japan;
 
 %% Calculate shortest path
 % get coordinates of each node
@@ -30,7 +32,7 @@ bounds = Custom2;
 [nodeMatrix, numNodes] = removeLandNodes([(1:numNodes)' coordinates], numNodes);
 
 % calculate shortest path after calculating closest nodes to input cities
-[p1, p2] = closestNodes3D(Houston, TelAviv, nodeMatrix);
+[p1, p2] = closestNodes3D(TelAviv, Osaka, nodeMatrix);
 [path] = genPath(nodeMatrix, [p1 p2], resolution);
 
 
